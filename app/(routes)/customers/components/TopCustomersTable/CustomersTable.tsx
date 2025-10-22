2ef190232f85b344d9f4908a5c67430e0e6ea1d2
leoaguinaga <u21219471@utp.edu.pe>
2025-10-22 14:34:42 -0500

    fix: Update data source in CustomersTable to use topCustomersData

--- app/(routes)/customers/components/TopCustomersTable/CustomersTable.tsx
+++ app/(routes)/customers/components/TopCustomersTable/CustomersTable.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import DataTable from './data-table'
 import { columns } from './columns'
-import { customers } from './CustomersTable.data'
+import { topCustomersData } from './CustomersTable.data'
 
 export default function CustomersTable() {
   return (
     <div className='w-full'>
-        <DataTable columns={columns} data={customers} />
+        <DataTable columns={columns} data={topCustomersData} />
     </div>
   )
 }
